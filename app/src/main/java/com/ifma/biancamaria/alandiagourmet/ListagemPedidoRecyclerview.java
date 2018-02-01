9dce251bea48edc76a1b874e9d6c9b70209c06ba
Si Rodrigues <sigferodrigues@gmail.com>
2018-01-31 23:49:50 -0300

    Add files via upload

--- app/src/main/java/com/ifma/biancamaria/alandiagourmet/ListagemPedidoRecyclerview.java
+++ app/src/main/java/com/ifma/biancamaria/alandiagourmet/ListagemPedidoRecyclerview.java
@@ -1,36 +1,36 @@
 package com.ifma.biancamaria.alandiagourmet;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.os.Bundle;
 
-import adapters.AdapterCliente;
-import dao.ClienteDao;
+import adapters.AdapterPedido;
+import dao.PedidoDao;
 
-public class ListagemClientesRecyclerview extends AppCompatActivity {
+public class ListagemPedidoRecyclerview extends AppCompatActivity {
 
     private RecyclerView meurecyclerview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.listagem_clientes);
+        setContentView(R.layout.listagem_pedido);
 
-        meurecyclerview = (RecyclerView) findViewById(R.id.rcvListagemdeClientes);
+        meurecyclerview = (RecyclerView) findViewById(R.id.rcvListagemPedido);
         LinearLayoutManager nossolayout = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
 
         meurecyclerview.setLayoutManager(nossolayout);
 
-        ClienteDao dao = new ClienteDao(this);
+        PedidoDao dao = new PedidoDao(this);
 
-        AdapterCliente meuadapter = new AdapterCliente(this,dao.listar());
+        AdapterPedido meuadapter = new AdapterPedido(this,dao.listar());
 
         meurecyclerview.setAdapter(meuadapter);
 
     }
 
-    public void adicionaCliente(){
+    public void adicionaPedido(){
 
     }
 
--- app/src/main/java/dao/PedidoDao.java
+++ app/src/main/java/dao/PedidoDao.java
@@ -62,4 +62,50 @@ public class PedidoDao {
 
     }
 
+    public boolean alterar(Pedido pedido){
+        long resultado = 0;
+        boolean retorno = false;
+
+        ContentValues valores = new ContentValues();
+        valores.put("tipo",pedido.getTipo());
+        valores.put("sabor",pedido.getSabor());
+        valores.put("tamanho",pedido.getTamanho());
+
+        resultado = gw.getDatabase().update("pedido",valores,"idpedido=?", new String[]{"" + String.valueOf(pedido.getIdpedido())});
+
+        if (resultado > 0)
+            retorno = true;
+        return retorno;
+    }
+
+    public boolean excluir(Pedido pedido){
+
+        long resultado = 0;
+        boolean retorno = false;
+
+        resultado = gw.getDatabase().delete("pedido","idpedido=?",
+                new String[]{"" + String.valueOf(pedido.getIdpedido())});
+        if (resultado > 0)
+            retorno = true;
+        return retorno;
+    }
+
+    public Pedido retornaUltimo(){
+
+        String colunas[] = {"tipo","sabor","tamanho","idpedido"};
+        cursor = gw.getDatabase().query("pedido",colunas,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            Pedido pe = new Pedido();
+            pe.setTipo(cursor.getString(0));
+            pe.setSabor(cursor.getString(1));
+            pe.setTamanho(cursor.getString(2));
+            pe.setIdpedido(cursor.getInt(3));
+            cursor.close();
+            return pe;
+        }
+
+        return null;
+    }
+
+
 }
